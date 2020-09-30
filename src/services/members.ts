48e3d319817902df28f4fb69c0e6fd2a20c19c1c
Aaron Osher <git@aaronosher.io>
2020-10-01 00:56:25 +0100

    Add Get All Members

--- src/services/members.ts
+++ src/services/members.ts
@@ -1,6 +1,9 @@
 import { GQLMember, Member } from '../types/Member';
+import _ from 'lodash';
 import client from './db';
 import { gql } from 'graphql-request';
+
+const members: { [key: number]: Member } = {};
 
 export const getMemberById = async (id: number): Promise<Member> => {
   const query = gql`
@@ -21,22 +24,72 @@ export const getMemberById = async (id: number): Promise<Member> => {
   return data.member;
 };
 
-export const addMember = async (member: Member): Promise<undefined> => {
+export const getAllMembers = async (): Promise<Member[]> => {
   try {
     const query = gql`
-      mutation AddMemberMutation($id: Int!, $name: String!, $type: String!, $joined: String!) {
-        createMember(data: { id: $id, name: $name, type: $type, joined: $joined }) {
-          id
+      query GetAllMembersQuery {
+        allMembers(_size: 1000) {
+          members: data {
+            id
+            name
+            joined
+            type
+            discord
+          }
         }
       }
     `;
 
-    await client.request<{ id: number }, Member>(query, member);
-    return;
+    const {
+      allMembers: { members },
+    } = await client.request<{ allMembers: { members: Member[] } }>(query);
+
+    return members;
+  } catch (e) {
+    throw e;
+  }
+};
+
+export const addMember = async (member: Member): Promise<Member> => {
+  try {
+    const query = gql`
+      mutation AddMemberMutation($id: Int!, $name: String!, $type: String!, $joined: String!) {
+        member: createMember(data: { id: $id, name: $name, type: $type, joined: $joined }) {
+          id
+          name
+          type
+          joined
+          discord
+        }
+      }
+    `;
+
+    const { member: savedMember } = await client.request<{ member: Member }, Member>(query, member);
+    return savedMember;
   } catch (e) {
     if (e.message.substr(0, 23) === 'Instance is not unique.') {
-      return;
+      return member;
     }
+    throw e;
+  }
+};
+
+export const addMemberIfChanged = async (member: Member): Promise<Member> => {
+  try {
+    if (!members[member.id]) {
+      const savedMember = await addMember(member);
+      members[savedMember.id] = savedMember;
+      return savedMember;
+    }
+
+    if (!_.isEqual(members[member.id], member)) {
+      const savedMember = await updateMember(member.id, member);
+      members[savedMember.id] = savedMember;
+      return savedMember;
+    }
+
+    return members[member.id];
+  } catch (e) {
     throw e;
   }
 };
@@ -59,8 +112,18 @@ export const updateMember = async (id: number, member: Partial<Member>): Promise
     const { member: originalMember } = await client.request<{ member: GQLMember }, { id: number }>(getQuery, { id });
 
     const updateQuery = gql`
-      mutation UpdateMemberMutation($_id: ID!, $id: Int!, $name: String!, $type: String!, $joined: !String, $discord: String) {
-        member: updateMember(id: $_id, data: { id: $id, name: $name, type: $type, joined: $joined, discord: $discord }) {
+      mutation UpdateMemberMutation(
+        $_id: ID!
+        $id: Int!
+        $name: String!
+        $type: String!
+        $joined: String!
+        $discord: String
+      ) {
+        member: updateMember(
+          id: $_id
+          data: { id: $id, name: $name, type: $type, joined: $joined, discord: $discord }
+        ) {
           id
           name
           type
@@ -70,9 +133,10 @@ export const updateMember = async (id: number, member: Partial<Member>): Promise
       }
     `;
 
-    const { member: newMember } = await client.request<{ member: Member }, { _id: string; data: Member }>(updateQuery, {
-      _id: originalMember._id,
-      data: { ...originalMember, ...member },
+    console.log('_id: ', originalMember._id, 'data: ', { ...originalMember, ...member });
+
+    const { member: newMember } = await client.request<{ member: Member }, GQLMember>(updateQuery, {
+      ...{ ...originalMember, ...member },
     });
 
     return newMember;
