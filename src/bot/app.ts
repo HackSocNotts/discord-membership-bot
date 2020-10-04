2ed83851a4997ed873e41ba688f94b73689f21df
Aaron Osher <git@aaronosher.io>
2020-10-04 07:59:46 +0100

    Add heartbeat support

--- src/bot/app.ts
+++ src/bot/app.ts
@@ -4,6 +4,7 @@ import './clearUser';
 import './clearAll';
 import './verify';
 import './verifyAll';
+import { beat } from '../services/heartbeat';
 import client from './client';
 import { ClientUser } from 'discord.js';
 
@@ -12,3 +13,15 @@ client.login(process.env.discordBotToken);
 client.on('ready', () => {
   console.log(`Logged in as ${(client.user as ClientUser).tag}!`);
 });
+
+const checkStatus = () => {
+  try {
+    if ((client.user as ClientUser).presence.status === 'online') {
+      beat();
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+setInterval(checkStatus, 1 * 60 * 1000);
