20ffae28d401f625d6db194046681a4533f7fa1f
Aaron Osher <git@aaronosher.io>
2020-10-21 07:30:08 +0100

    Two Hotfixes (#9)
    * patch capitalisation issue in clearAll env var
    
    * patch permissions error on manual verify

--- src/bot/manualVerify.ts
+++ src/bot/manualVerify.ts
@@ -1,6 +1,6 @@
 import { getMemberByDiscord, getMemberById, updateMember } from '../services/members';
+import { GuildMember, Permissions } from 'discord.js';
 import client from './client';
-import { GuildMember } from 'discord.js';
 
 /**
  * Handle verification dm
@@ -16,45 +16,52 @@ client.on('message', async (message) => {
     const id = parseInt((message.content.match(commandPattern) as RegExpMatchArray)[1]);
     const snowflake = (message.content.match(commandPattern) as RegExpMatchArray)[2];
 
-    message.channel.startTyping();
-    try {
-      const memberFromDiscord = await getMemberByDiscord(snowflake);
-      const memberFromId = await getMemberById(id);
+    if (
+      client.user &&
+      message.mentions.has(client.user) &&
+      message.member &&
+      message.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)
+    ) {
+      try {
+        message.channel.startTyping();
+        const memberFromDiscord = await getMemberByDiscord(snowflake);
+        const memberFromId = await getMemberById(id);
 
-      if (memberFromDiscord || (memberFromId && memberFromId.discord)) {
-        message.channel.stopTyping();
-        message.reply(`It looks like either that Discord account or student ID is already verified.`);
-        return;
-      }
+        if (memberFromDiscord || (memberFromId && memberFromId.discord)) {
+          message.channel.stopTyping();
+          message.reply(`It looks like either that Discord account or student ID is already verified.`);
+          return;
+        }
 
-      if (!memberFromId) {
+        if (!memberFromId) {
+          message.channel.stopTyping();
+          message.reply(
+            `I can't find a member with the ID number \`${id}\`. Please make sure you entered the ID correctly.`,
+          );
+          return;
+        }
+
+        await updateMember(id, { discord: snowflake });
+
+        const guild = await client.guilds.fetch(process.env.discordGuildID as string);
+        const members = await guild.members.fetch();
+        const member = members.get(snowflake) as GuildMember;
+        await member.roles.add(process.env.discordMemberRoleID as string);
+
         message.channel.stopTyping();
         message.reply(
-          `I can't find a member with the ID number \`${id}\`. Please make sure you entered the ID correctly.`,
+          `<@${snowflake}> has been verified as \`${id}\` successfully and should see all the member channels now.`,
         );
+      } catch (e) {
+        message.channel.stopTyping();
+        message.reply(
+          `An error occurred, please try again later, or raise a ticket in <#${
+            process.env.discordHelpChannelID as string
+          }> for help.`,
+        );
+        console.error(e);
         return;
       }
-
-      await updateMember(id, { discord: snowflake });
-
-      const guild = await client.guilds.fetch(process.env.discordGuildID as string);
-      const members = await guild.members.fetch();
-      const member = members.get(snowflake) as GuildMember;
-      await member.roles.add(process.env.discordMemberRoleID as string);
-
-      message.channel.stopTyping();
-      message.reply(
-        `<@${snowflake}> has been verified as \`${id}\` successfully and should see all the member channels now.`,
-      );
-    } catch (e) {
-      message.channel.stopTyping();
-      message.reply(
-        `An error occurred, please try again later, or raise a ticket in <#${
-          process.env.discordHelpChannelID as string
-        }> for help.`,
-      );
-      console.error(e);
-      return;
     }
   }
 });
